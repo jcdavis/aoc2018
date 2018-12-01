c55cbdf9bd957b23d92f1506b77ea06356786a8a
Jackson Davis <jackson@jcdav.is>
2018-11-30 22:03:44 -0800

    day1 p2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,20 +1,30 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Result};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("Hello, world!");
     let f = File::open(&args[1]).unwrap();
     let br = BufReader::new(f);
     let mut sum = 0;
-    for line in br.lines() {
-      let l = line.unwrap();
-      let delta = l[1..].parse::<i32>().unwrap();
-      if l.chars().next().unwrap() == '+' {
-        sum += delta;
-      } else {
-        sum -= delta;
+    let lines: Vec<Result<String>> = br.lines().collect();
+    let mut seen: HashSet<i32> = HashSet::new();
+    let mut shouldContinue = true;
+    while shouldContinue {
+      for line in &lines {
+        if seen.contains(&sum) {
+          shouldContinue = false;
+          break;
+        }
+        seen.insert(sum);
+        let l = line.as_ref().unwrap();
+        let delta = l[1..].parse::<i32>().unwrap();
+        if l.chars().next().unwrap() == '+' {
+          sum += delta;
+        } else {
+          sum -= delta;
+        }
       }
     }
     println!("Sum: {}", sum);
