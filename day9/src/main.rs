623eaac39e3e5d7b61f8473ab6e37ac36fd334c1
Jackson Davis <jackson@jcdav.is>
2018-12-13 21:13:07 -0800

    day9 p2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -3,26 +3,42 @@ fn main() {
     println!("{}", score(404, 7185200));
 }
 
-fn score(players: usize, last: i32) -> i32 {
-    let mut circle = vec![0, 1];
-    let mut current_i = 1;
-    let mut scores = [0; 1000];
+//Double linked list using array indices because rust
+struct Position {
+    marble: i32,
+    prev: usize,
+    next: usize,
+}
+
+fn score(players: usize, last: i32) -> i64 {
+    let mut circle: Vec<Position> = Vec::new();
+    circle.push(Position {marble: 0, prev: 0, next: 0});
+    let mut current_i = 0;
+    let mut scores: [i64; 1000] = [0; 1000];
     let mut current_p = 0;
 
-    for marble in 2..(last+1) {
+    for marble in 1..(last+1) {
         if marble % 23 != 0 {
-            let idx = (current_i + 2) % circle.len();
-            circle.insert(idx, marble);
-            current_i = idx;
+            let b_i = circle[current_i].next;
+            let a_i = circle[b_i].next;
+            let new_i = circle.len();
+            circle[a_i].prev = new_i;
+            circle[b_i].next = new_i;
+            circle.push(Position {marble: marble, prev: b_i, next: a_i});
+            current_i = new_i;
         } else {
-            scores[current_p] += marble;
-            let removed_i = (current_i + circle.len() - 7) % circle.len();
-            let removed = circle.remove(removed_i);
-            scores[current_p] += removed;
-            current_i = removed_i % circle.len();
-            //println!("{} got {} + {}", current_p, marble, removed);
+            scores[current_p] += marble as i64;
+            let mut removed_i = current_i;
+            for _ in 0..7 {
+                removed_i = circle[removed_i].prev;
+            }
+            scores[current_p] += circle[removed_i].marble as i64;
+            let p_i = circle[removed_i].prev;
+            let n_i = circle[removed_i].next;
+            circle[p_i].next = n_i;
+            circle[n_i].prev = p_i;
+            current_i = n_i;
         }
-        //println!("{:?}", circle);
         current_p = (current_p + 1) % players;
     }
     return *scores.iter().max().unwrap();
